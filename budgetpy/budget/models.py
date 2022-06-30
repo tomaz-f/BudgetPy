a70efe5e58f0a699517363a751008ab0be20e5a8
tomaz-f <tomazalvesfelipe@gmail.com>
2022-06-30 10:57:08 -0400

    create the main django models

--- budgetpy/budget/models.py
+++ budgetpy/budget/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, unique = True, blank = True)
+    budget = models.IntegerField()
