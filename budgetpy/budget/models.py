fd3e91d9c2e4356e27e81fb06e28e1e161ad02d8
tomaz-f <tomazalvesfelipe@gmail.com>
2022-07-07 14:25:14 -0400

    implement the functionality for removing expenses and do some miscellaneous tasks, such as displaying the correct budget left and total transactions.

--- budgetpy/budget/models.py
+++ budgetpy/budget/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 
+
 class Project(models.Model):
     name = models.CharField(max_length=100)
     slug = models.CharField(max_length=100, unique = True, blank = True)
@@ -9,6 +10,20 @@ class Project(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Project, self).save(*args, **kwargs)
+
+    def budget_left(self):
+        expense_list = Expense.objects.filter(project=self)
+        total_expense_amount = 0
+        for expense in expense_list:
+            total_expense_amount += expense.amount
+
+        return self.budget - total_expense_amount
+
+
+    def total_transactions(self):
+        expense_list = Expense.objects.filter(project=self)
+        return len(expense_list)
+
 
 class Category(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
